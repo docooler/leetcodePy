b7eb0d3ad41f7d484024777860e7ba809afebe39
docooler <laiyuan.yang@gmail.com>
2014-10-20 19:35:38 +0800

    [docooler] change test to unitest

--- siglenum.py
+++ siglenum.py
@@ -1,7 +1,10 @@
+import unittest
+
 class Solution:
 	# @param A, a list of integer
     # @return an integer
 	def singleNumber(self, A):
+
 		return 7
 
 class Solution_test:
@@ -18,16 +21,20 @@ class Solution_test:
         	if value == 1:
         		return key
 
-def main():
-	data = [1,2,3,4,5,6,7,6,5,4,3,2,1]
-	s = Solution()
-	s_test = Solution_test()
-	d_test = s_test.singleNumber(data)
-	d = s.singleNumber(data)
-	if d != d_test:
-		print "Error"
-	else:
-		print "Done"
+class SolutionUnitTest(unittest.TestCase):
+	"""docstring for ClassName"""
+	def setup(self):
+		pass
+	def tearDown(self):
+		pass
+	def testsingleNumber(self):
+		data = [1,2,3,4,5,6,7,6,5,4,3,2,1]
+
+		s_test = Solution_test()
+		s      = Solution()
+		self.assertEqual(s.singleNumber(data),s_test.singleNumber(data), "test failed")
+
+
 
 if __name__ == '__main__':
-	main()
+	unittest.main()
