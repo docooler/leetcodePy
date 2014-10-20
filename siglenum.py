12899732b068bef8f25b6e992637336a4eff6368
docooler <laiyuan.yang@gmail.com>
2014-10-20 19:18:12 +0800

    [docooler] init check func

---
+++ siglenum.py
@@ -0,0 +1,21 @@
+class Solution:
+    # @param A, a list of integer
+    # @return an integer
+    def singleNumber(self, A):
+        datamap = {}
+        for i in A:
+        	if datamap.has_key(i):
+        		datamap[i] = 2
+        	else:
+        		datamap[i] = 1
+        for (key, value) in datamap.items():
+        	if value == 1:
+        		return key
+
+def main():
+	data = [1,2,3,4,5,6,7,6,5,4,3,2,1]
+	s = Solution()
+	print s.singleNumber(data)
+
+if __name__ == '__main__':
+	main()
