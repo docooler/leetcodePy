6126fc49d4c516eca4b448432f69e85e7878b3c7
docooler <laiyuan.yang@gmail.com>
2014-10-21 21:47:51 +0800

    [docooler] add find min

---
+++ findMin.py
@@ -0,0 +1,35 @@
+import unittest
+
+
+# f(0) = [0]
+# f(1) = [0,1]
+# f(n+1) = duplicate(f(n)) 
+
+class Solution:
+	# @param A, a list of integer
+    # @return an integer
+	def findMin(self, num):
+		min_data = num[0]
+		for x in num:
+			if min_data > x:
+				min_data = x
+		return min_data
+
+class SolutionUnitTest(unittest.TestCase):
+	"""docstring for SolutionUnitTest"""
+	def setup(self):
+		pass
+	def tearDown(self):
+		pass
+	def testsingleNumber(self):
+		# data = [0,1,3,2]
+		s = Solution()
+		print s.findMin([4,5,6,7,0,1,2,3])
+		# print str(len(r_data))
+        # print r_data
+		# self.assertEqual(r_data ,data, "test failed")
+
+
+
+if __name__ == '__main__':
+	unittest.main()
