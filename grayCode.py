52b60a31737de1377645308e10f347f2535c6c30
docooler <docooler@njust.edu.cn>
2014-10-21 11:04:40 +0800

    Update grayCode.py
    change list to set

--- grayCode.py
+++ grayCode.py
@@ -4,21 +4,21 @@ class Solution:
 	# @param A, a list of integer
     # @return an integer
 	def grayCode(self, n):
-		self.used = []
+		self.used = set()
 		self.len  = n
 		self.data = []
 
 		# print "data len " + str(self.len)
 		current = 0
 		self.data.append(current)
-		self.used.append(current)
+		self.used.add(current)
 		# print "data length is " + str(len(self.data))
 		# print "all data num is " + str(1<<self.len)
 		while len(self.data) < (1<<self.len):		
 			current = self.getNext(current)
 			# if current == None:
 			# 	break
-			self.used.append(current)
+			self.used.add(current)
 			self.data.append(current)
 		# print self.data
 		return self.data
@@ -52,9 +52,7 @@ class SolutionUnitTest(unittest.TestCase):
 	def testsingleNumber(self):
 		# data = [0,1,3,2]
 		s = Solution()
-		for x in xrange(0,7):
-			print str(x) + "is :"
-			print s.grayCode(x)
+		s.grayCode(20)
 		# print str(len(r_data))
         # print r_data
 		# self.assertEqual(r_data ,data, "test failed")
