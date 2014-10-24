49a2342177e6c59850221bc2a9f37b180fca5a04
docooler <laiyuan.yang@gmail.com>
2014-10-24 22:05:49 +0800

    [docooler] max deep tree level to bottom

---
+++ maxDeepTreeLevel2Botom.py
@@ -0,0 +1,30 @@
+class Solution:
+    # @param root, a tree node
+    # @return a list of lists of integers
+    def maxDepth(self, root):
+        self.minDeep = None
+        if root == None:
+            return 0
+        if root.left != None:
+            self.treeDeepVisit(root.left, 1)
+        if root.right != None:
+            self.treeDeepVisit(root.right,1)
+
+        if self.minDeep == None:
+            return 1
+        return self.minDeep
+
+    def treeDeepVisit(self, root, parentDeep):
+        selfDeep = parentDeep + 1
+        if root.left == None and root.right == None:
+            if self.minDeep != None:
+                if self.minDeep < selfDeep:
+                    self.minDeep = selfDeep
+            else:
+                self.minDeep = selfDeep
+            return 
+        if root.left != None:
+            self.treeDeepVisit(root.left, selfDeep)
+        if root.right != None:
+            self.treeDeepVisit(root.right, selfDeep)
+        
