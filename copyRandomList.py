1d01a9471e6bce636eebbe5824798f0c5777308c
docooler <laiyuan.yang@gmail.com>
2014-10-26 22:18:12 +0800

    [docooler] add copy list with random pointer but time limit exceeded.

---
+++ copyRandomList.py
@@ -0,0 +1,71 @@
+# Definition for singly-linked list with a random pointer.
+
+import unittest
+
+class  SolutionTest(unittest.TestCase):
+    """docstring for  SolutionTest"""
+    def setup(self):
+        pass
+    def tearDown(self):
+        pass
+    def testCase1(self):
+        rList = RandomListNode(1)
+        rList.random = RandomListNode(2)
+
+        s= Solution()
+        s.copyRandomList(rList)
+    def  testCase2(self):
+        s = Solution()
+        r = s.copyRandomList(None)
+        self.assertEqual(r, None)
+
+class RandomListNode:
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
+
+class Solution:
+    # @param head, a RandomListNode
+    # @return a RandomListNode
+    def __init__(self):
+        self.nodeMap = {}
+    def copyRandomList(self, head):
+        nodeMap = self.nodeMap
+        randomList = []
+        if head == None:
+            return None
+            
+        temp = head
+        nHead = None
+        nPrev = None
+        while temp != None:
+            node = RandomListNode(temp.label)
+            if nodeMap.has_key(temp) == False:
+                nodeMap[temp] = node
+            if temp.random != None:
+                if nodeMap.has_key(temp.random) == False:
+                    randomList.append((node,temp))
+                else:
+                    node.random = nodeMap[temp.random]
+            
+            if nHead == None:
+                nHead = node
+                nPrev = node
+            else:
+                nPrev.next = node
+                nPrev = nPrev.next
+            temp = temp.next
+        while randomList != []:
+            node, srcNode = randomList.pop()
+            node.random = self.copyRandomList(srcNode.random)
+        return nHead
+if __name__ == '__main__':
+    unittest.main()
+
+        
+        
+                
+            
+            
+        
