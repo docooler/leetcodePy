8b36b136e1abf24de52ced82d4c526fb7f8eddf0
docooler <docooler@njust.edu.cn>
2014-10-28 23:50:49 +0800

    Update climbingStairs.py
    add unittest and save a lot of memory. memory usage is constants

--- climbingStairs.py
+++ climbingStairs.py
@@ -1,13 +1,31 @@
+import unittest
+
 class Solution:
     # @param n, an integer
     # @return an integer
     def climbStairs(self, n):
-        if n < 2:
+        if n == 1:
             return 1
-        ways = [0] * (n+1)
-        ways[0] = 0
-        ways[1] = 1
-        ways[2] = 2
-        for i in range(3, n+1):
-            ways[i] = ways[i-2] + ways[i-1]
-        return ways[n]
+        if n == 2:
+            return 2
+
+        first   = 1
+        second  = 2
+        for i in range(n-2):
+            first, second = second, second + first
+
+        return second
+class SolutionTest(unittest.TestCase):
+    """docstring for SolutionTest"""
+    def setup(self):
+        pass
+    def tearDown(self):
+        pass
+    def testCase1(self):
+        s = Solution()
+        self.assertEquals(s.climbStairs(1), 1)
+        self.assertEquals(s.climbStairs(2), 2)
+        self.assertEquals(s.climbStairs(3), 3)
+if __name__ == '__main__':
+    unittest.main()
+        
