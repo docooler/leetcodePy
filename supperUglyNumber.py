2f062b2beed3567f1f52443528923d25d2092205
docooler <docooler@njust.edu.cn>
2016-01-27 11:17:19 +0800

    Update supperUglyNumber.py

--- supperUglyNumber.py
+++ supperUglyNumber.py
@@ -18,3 +18,22 @@ class Solution(object):
                     idxList[k]=idxList[k]+1
 
         return lists[n-1]
+        
+        
+class Solution(object):
+    def nthSuperUglyNumber(self, n, primes):
+    
+        lists = []
+        lists.append(1)
+        pLen = len(primes)
+        idxList = [0] * pLen
+        tempList = primes[:]
+        for i in xrange(1, n):
+            tempMin = min(tempList)
+            lists.append(tempMin)
+            for k, v in enumerate(tempList):
+                if v == tempMin:
+                    idxList[k]=idxList[k]+1
+                    tempList[k]=lists[idxList[k]]*primes[k]
+
+        return lists[n-1]
